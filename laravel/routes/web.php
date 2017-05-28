bffa0d3ff61ce3fc93c4bfa1785d3f41a2c07a34
Mathieu Claessens <mathieu_claessens@hotmail.com>
2017-05-28 08:22:35 +0200

    laravel first draw - Setup routes

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,34 +10,38 @@
 |
 */
 
-Route::get( '/', 'UserController@index' );
-Route::get( 'conseil-administration', 'RoleController@ca' );
-Route::get( 'entraineurs', 'RoleController@trainer' );
-Route::get( 'equipes', 'TeamController@index' );
-Route::get( 'equipes/{equipe}', 'TeamController@show' );
-Route::get( 'reglement' );
-Route::get( 'calendrier' );
-Route::get( 'albums', 'PhotoController@album_index' );
-Route::get( 'albums/{album}', 'PhotoController@album_show' );
-Route::get( 'telechargements' );
-Route::get( 'complexe' );
-Route::get( 'coaching' );
-Route::get( 'evenements/{evenement}', 'EventController@show' );
-Route::get( 'identification' );
-Route::get( 'users/', 'UserController@index' );
-Route::get( 'users/{user}', 'UserController@show' );
-Route::get( 'contact', function () {
-	return view( 'contact' );
-} );
-// Authentication
-Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
+// ******** Administration routes ********
+Route::get( '/admin', 'AdminController@index' );
+Route::get( '/admin/family', 'FamilyController@index' );
+Route::get( '/admin/family/{family_slug}', 'FamilyController@show' );
 
-// Import from excel
+// ******** Front end routes ********
+Route::get( '/', 'PageController@home' );
+// ******** Authentication ********
+Auth::routes();
+Route::get('/connected', 'PageController@connected')->name('connected');
+
+// Route::get( 'conseil-administration', 'RoleController@ca' );
+// Route::get( 'entraineurs', 'RoleController@trainer' );
+// Route::get( 'equipes', 'TeamController@index' );
+// Route::get( 'equipes/{equipe}', 'TeamController@show' );
+// Route::get( 'reglement' );
+// Route::get( 'calendar', 'GameController@index' );
+// Route::get( 'albums', 'PhotoController@album_index' );
+// Route::get( 'albums/{album}', 'PhotoController@album_show' );
+// Route::get( 'telechargements' );
+// Route::get( 'complexe' );
+// Route::get( 'coaching' );
+// Route::get( 'evenements/{evenement}', 'EventController@show' );
+// Route::get( 'identification' );
+Route::get( 'users/{user}', 'UserController@show' )->name('profil');
+Route::get( '/contact', 'PageController@contact' )->name('contact');
+
+// ******** Import from excel ********
 Route::get('import-export-csv-excel',array('as'=>'excel.import','uses'=>'FileController@importExportExcelORCSV'));
 Route::post('import-csv-excel',array('as'=>'import-csv-excel','uses'=>'FileController@importFileIntoDB'));
 Route::get('download-excel-file/{type}', array('as'=>'excel-file','uses'=>'FileController@downloadExcelFile'));
 
-// temp
+// ******** Temp ********
 Route::get( 'role/', 'RoleController@index' );
 Route::get( 'role/{role}', 'RoleController@show' );
