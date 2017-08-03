b8dc11fa0b07b6540eea5b2f47564ebef4d37466
Mathieu Claessens <mathieu_claessens@hotmail.com>
2017-08-03 16:09:58 +0200

    Update to responsive for single event page

--- laravel/resources/views/event/show.blade.php
+++ laravel/resources/views/event/show.blade.php
@@ -5,18 +5,24 @@
 <div class="container">
     <section class="section">
         <h2 class="section-title"><span class="section-icon section-icon-news"></span>{{$event->title}}</h2>
-        <div class="section-content event">
+        <div class="section-content event flex-wrap">
         	<div class="event-photo">
 	        	<img src="{{$event->src}}" srcset="{{$event->srcset}}" alt="Photo de l'event">
         	</div>
         	<div class="event-body">
 		        {!!$event->description!!}
-        		<h3>Quand ?</h3>
-        		<p>{{$event->date}}</p>
-        		<h3>Où ?</h3>
-        		<p>{{$event->location}}</p>
-        		<h3>Réservation</h3>
-        		<p>{{$event->reservation}}</p>
+		        <div class="event-sub-section">
+        			<h3>Quand ?</h3>
+        			<p>{{$event->date}}</p>
+        		</div>
+		        <div class="event-sub-section">
+        			<h3>Où ?</h3>
+        			<p>{{$event->location}}</p>
+        		</div>
+		        <div class="event-sub-section">
+        			<h3>Réservation</h3>
+        			<p>{{$event->reservation}}</p>
+        		</div>
 		    </div>
         </div>
     </section>
